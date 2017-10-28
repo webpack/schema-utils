9f01816f4c1427e8b04b46b0a350d66201a56d7f
Sergey <gribnoysup@users.noreply.github.com>
2017-10-28 23:01:04 +0200

    feat: add support for `typeof`, `instanceof` (`{Function\|RegExp}`) (#10)

--- src/validateOptions.js
+++ src/validateOptions.js
@@ -2,15 +2,18 @@ import fs from 'fs';
 import path from 'path'; // eslint-disable-line
 
 import Ajv from 'ajv';
+import ajvKeywords from 'ajv-keywords';
 import ValidationError from './ValidationError';
 
-const validateOptions = (schema, options, name) => {
-  const ajv = new Ajv({
-    useDefaults: true,
-    allErrors: true,
-    errorDataPath: 'property',
-  });
+const ajv = new Ajv({
+  useDefaults: true,
+  allErrors: true,
+  errorDataPath: 'property',
+});
 
+ajvKeywords(ajv, ['instanceof', 'typeof']);
+
+const validateOptions = (schema, options, name) => {
   if (typeof schema === 'string') {
     schema = fs.readFileSync(path.resolve(schema), 'utf8'); // eslint-disable-line
     schema = JSON.parse(schema); // eslint-disable-line
--- test/index.test.js
+++ test/index.test.js
@@ -6,21 +6,41 @@ test('Valid', () => {
     string: 'hello',
     array: [ 'a' ],
     object: { prop: false },
-    boolean: true
+    boolean: true,
+    type: function() {},
+    instance: new RegExp(''),
+
   };
 
   expect(validateOptions('test/fixtures/schema.json', options, 'Loader'))
     .toBe(true);
 });
 
-test('Error', () => {
+describe('Error', () => {
   const options = {
     string: false,
     array: {},
     object: { prop: 1 },
-    boolean: 'hello'
+    boolean: 'hello',
+    type: null,
+    instance: function() {},
   };
 
-  expect(() => validateOptions('test/fixtures/schema.json', options, '{Name}'))
-    .toThrowError(/Validation Error\n\n{Name} Invalid Options\n\n/);
+  const validate = () => validateOptions('test/fixtures/schema.json', options, '{Name}')
+
+  test('should throw error', () => {
+    expect(validate).toThrowError(/Validation Error\n\n{Name} Invalid Options\n\n/);
+  })
+
+  test('should have errors for every key in options', () => {
+    try {
+      validate()
+    } catch(error) {
+      const expected = ['.string', '.array', '.object.prop', '.boolean', '.type', '.instance'];
+      const errors = error.err.map(e => e.dataPath)
+
+      expect(errors).toMatchObject(expected);
+      expect(error.err).toMatchSnapshot();
+    }
+  })
 });
