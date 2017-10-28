6d4e036bc1e18565c6e830b9c9866a6c71f52d12
Sergey Petushkov <petushkov.sergey@gmail.com>
2017-10-28 16:29:56 +0200

    Add support for typeof, instanceof; Update tests

--- test/index.test.js
+++ test/index.test.js
@@ -6,21 +6,40 @@ test('Valid', () => {
     string: 'hello',
     array: [ 'a' ],
     object: { prop: false },
-    boolean: true
+    boolean: true,
+    type: function() {},
+    instance: new RegExp(''),
+
   };
 
   expect(validateOptions('test/fixtures/schema.json', options, 'Loader'))
     .toBe(true);
 });
 
-test('Error', () => {
+describe('Error', () => {
   const options = {
     string: false,
     array: {},
     object: { prop: 1 },
-    boolean: 'hello'
+    boolean: 'hello',
+    type: null,
+    instance: function() {},
   };
 
-  expect(() => validateOptions('test/fixtures/schema.json', options, '{Name}'))
-    .toThrowError(/Validation Error\n\n{Name} Invalid Options\n\n/);
+  const test = () => validateOptions('test/fixtures/schema.json', options, '{Name}')
+
+  it('should throw error', () => {
+    expect(test).toThrowError(/Validation Error\n\n{Name} Invalid Options\n\n/);
+  })
+
+  it('should have errors for every key in options', () => {
+    try {
+      test()
+    } catch(error) {
+      const expected = ['.string', '.array', '.object.prop', '.boolean', '.type', '.instance'];
+      const errors = error.err.map(e => e.dataPath)
+
+      expect(errors).toMatchObject(expected)
+    }
+  })
 });
