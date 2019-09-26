f71adbdbc92d0ead814bd319c722eec4f160c4d2
evilebottnawi <sheo13666q@gmail.com>
2019-09-26 21:55:42 +0300

    test: more

--- test/api.test.js
+++ test/api.test.js
@@ -1,8 +1,34 @@
 import schemaUtils from '../src/index';
 
+import schema from './fixtures/schema.json';
+
 describe('api', () => {
   it('should export validate and ValidateError', () => {
     expect(typeof schemaUtils).toBe('function');
     expect(typeof schemaUtils.ValidateError).toBe('function');
+  });
+
+  it('should work', () => {
+    schemaUtils(schema, { minimumWithTypeNumber: 5 });
+  });
+
+  it('should work when options will be changed', () => {
+    expect.assertions(1);
+
+    const options = { minimumWithTypeNumber: 5 };
+
+    schemaUtils(schema, options);
+
+    options.minimumWithTypeNumber = 1;
+
+    try {
+      schemaUtils(schema, options);
+    } catch (error) {
+      expect(error).toBeDefined();
+    }
+
+    options.minimumWithTypeNumber = 120;
+
+    schemaUtils(schema, options);
   });
 });
