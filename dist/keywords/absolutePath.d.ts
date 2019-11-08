7b41f1ec6f0be3ecd7b023bdd3d6a5db052bbee3
Ivan Kopeykin <ikopeykin@ozon.ru>
2019-11-08 17:05:18 +0300

    feat: add declarations to git

---
+++ dist/keywords/absolutePath.d.ts
@@ -0,0 +1,11 @@
+export default addAbsolutePathKeyword;
+export type Ajv = import("ajv").Ajv;
+export type SchemaUtilErrorObject = import("ajv").ErrorObject & {
+    children?: import("ajv").ErrorObject[] | undefined;
+};
+/**
+ *
+ * @param {Ajv} ajv
+ * @returns {Ajv}
+ */
+declare function addAbsolutePathKeyword(ajv: import("ajv").Ajv): import("ajv").Ajv;
