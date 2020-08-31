2f40154b91e45b393258ae9dd8f10cc3b8590b7d
Alex Leung <alex.l.leung@gmail.com>
2020-08-31 03:45:51 -0700

    fix: remove esModuleInterop from tsconfig (#110) (#111)

--- src/validate.js
+++ src/validate.js
@@ -1,9 +1,10 @@
-import Ajv from 'ajv';
-import ajvKeywords from 'ajv-keywords';
-
 import addAbsolutePathKeyword from './keywords/absolutePath';
 
 import ValidationError from './ValidationError';
+
+// Use CommonJS require for ajv libs so TypeScript consumers aren't locked into esModuleInterop (see #110).
+const Ajv = require('ajv');
+const ajvKeywords = require('ajv-keywords');
 
 /** @typedef {import("json-schema").JSONSchema4} JSONSchema4 */
 /** @typedef {import("json-schema").JSONSchema6} JSONSchema6 */
