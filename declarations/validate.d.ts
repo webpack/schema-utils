e13bc8f713ef16985d50f33164ca53df1fe96bf2
Nitin Kumar <snitin315@gmail.com>
2021-05-25 17:04:41 +0530

    chore: update

--- declarations/validate.d.ts
+++ declarations/validate.d.ts
@@ -1,19 +1,16 @@
 export type JSONSchema4 = import('json-schema').JSONSchema4;
 export type JSONSchema6 = import('json-schema').JSONSchema6;
 export type JSONSchema7 = import('json-schema').JSONSchema7;
-export type ErrorObject = Ajv.ErrorObject;
+export type ErrorObject = import('ajv').ErrorObject;
 export type Extend = {
   formatMinimum?: number | undefined;
   formatMaximum?: number | undefined;
   formatExclusiveMinimum?: boolean | undefined;
   formatExclusiveMaximum?: boolean | undefined;
 };
-export type Schema =
-  | (import('json-schema').JSONSchema4 & Extend)
-  | (import('json-schema').JSONSchema6 & Extend)
-  | (import('json-schema').JSONSchema7 & Extend);
-export type SchemaUtilErrorObject = Ajv.ErrorObject & {
-  children?: Ajv.ErrorObject[] | undefined;
+export type Schema = (JSONSchema4 | JSONSchema6 | JSONSchema7) & Extend;
+export type SchemaUtilErrorObject = ErrorObject & {
+  children?: Array<ErrorObject>;
 };
 export type PostFormatter = (
   formattedError: string,
@@ -36,5 +33,4 @@ export function validate(
   configuration?: ValidationErrorConfiguration | undefined
 ): void;
 import ValidationError from './ValidationError';
-import Ajv = require('ajv');
 export { ValidationError };
