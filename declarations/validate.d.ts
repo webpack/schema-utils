e6842ff6272a5d707aa4aae5f33bec93e3896966
evilebottnawi <sheo13666q@gmail.com>
2021-10-22 14:23:16 +0300

    chore(deps): update

--- declarations/validate.d.ts
+++ declarations/validate.d.ts
@@ -3,24 +3,21 @@ export type JSONSchema6 = import("json-schema").JSONSchema6;
 export type JSONSchema7 = import("json-schema").JSONSchema7;
 export type ErrorObject = import("ajv").ErrorObject;
 export type Extend = {
-  formatMinimum?: number | undefined;
-  formatMaximum?: number | undefined;
-  formatExclusiveMinimum?: boolean | undefined;
-  formatExclusiveMaximum?: boolean | undefined;
-  link?: string | undefined;
+    formatMinimum?: number | undefined;
+    formatMaximum?: number | undefined;
+    formatExclusiveMinimum?: boolean | undefined;
+    formatExclusiveMaximum?: boolean | undefined;
+    link?: string | undefined;
 };
 export type Schema = (JSONSchema4 | JSONSchema6 | JSONSchema7) & Extend;
 export type SchemaUtilErrorObject = ErrorObject & {
-  children?: Array<ErrorObject>;
+    children?: Array<ErrorObject>;
 };
-export type PostFormatter = (
-  formattedError: string,
-  error: SchemaUtilErrorObject
-) => string;
+export type PostFormatter = (formattedError: string, error: SchemaUtilErrorObject) => string;
 export type ValidationErrorConfiguration = {
-  name?: string | undefined;
-  baseDataPath?: string | undefined;
-  postFormatter?: PostFormatter | undefined;
+    name?: string | undefined;
+    baseDataPath?: string | undefined;
+    postFormatter?: PostFormatter | undefined;
 };
 /**
  * @param {Schema} schema
@@ -28,10 +25,6 @@ export type ValidationErrorConfiguration = {
  * @param {ValidationErrorConfiguration=} configuration
  * @returns {void}
  */
-export function validate(
-  schema: Schema,
-  options: Array<object> | object,
-  configuration?: ValidationErrorConfiguration | undefined
-): void;
+export function validate(schema: Schema, options: Array<object> | object, configuration?: ValidationErrorConfiguration | undefined): void;
 import ValidationError from "./ValidationError";
 export { ValidationError };
