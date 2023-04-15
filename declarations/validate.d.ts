052b275518daa710c3b852cdd44b4691301de9b4
Alexander Akait <4567934+alexander-akait@users.noreply.github.com>
2023-04-15 17:34:24 +0300

    perf: initial start time (#168)

--- declarations/validate.d.ts
+++ declarations/validate.d.ts
@@ -22,6 +22,32 @@ export type ValidationErrorConfiguration = {
   baseDataPath?: string | undefined;
   postFormatter?: PostFormatter | undefined;
 };
+/** @typedef {import("json-schema").JSONSchema4} JSONSchema4 */
+/** @typedef {import("json-schema").JSONSchema6} JSONSchema6 */
+/** @typedef {import("json-schema").JSONSchema7} JSONSchema7 */
+/** @typedef {import("ajv").ErrorObject} ErrorObject */
+/**
+ * @typedef {Object} Extend
+ * @property {string=} formatMinimum
+ * @property {string=} formatMaximum
+ * @property {string=} formatExclusiveMinimum
+ * @property {string=} formatExclusiveMaximum
+ * @property {string=} link
+ */
+/** @typedef {(JSONSchema4 | JSONSchema6 | JSONSchema7) & Extend} Schema */
+/** @typedef {ErrorObject & { children?: Array<ErrorObject>}} SchemaUtilErrorObject */
+/**
+ * @callback PostFormatter
+ * @param {string} formattedError
+ * @param {SchemaUtilErrorObject} error
+ * @returns {string}
+ */
+/**
+ * @typedef {Object} ValidationErrorConfiguration
+ * @property {string=} name
+ * @property {string=} baseDataPath
+ * @property {PostFormatter=} postFormatter
+ */
 /**
  * @param {Schema} schema
  * @param {Array<object> | object} options
