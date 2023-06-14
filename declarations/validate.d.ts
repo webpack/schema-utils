02aa068df80d99cc576a5ed385f947eb5204c5db
Alexander Akait <4567934+alexander-akait@users.noreply.github.com>
2023-06-14 17:37:20 +0300

    fix(perf): cache compiled schema (#182)

--- declarations/validate.d.ts
+++ declarations/validate.d.ts
@@ -2,6 +2,7 @@ export type JSONSchema4 = import("json-schema").JSONSchema4;
 export type JSONSchema6 = import("json-schema").JSONSchema6;
 export type JSONSchema7 = import("json-schema").JSONSchema7;
 export type ErrorObject = import("ajv").ErrorObject;
+export type ValidateFunction = import("ajv").ValidateFunction;
 export type Extend = {
   formatMinimum?: number | undefined;
   formatMaximum?: number | undefined;
@@ -23,33 +24,6 @@ export type ValidationErrorConfiguration = {
   baseDataPath?: string | undefined;
   postFormatter?: PostFormatter | undefined;
 };
-/** @typedef {import("json-schema").JSONSchema4} JSONSchema4 */
-/** @typedef {import("json-schema").JSONSchema6} JSONSchema6 */
-/** @typedef {import("json-schema").JSONSchema7} JSONSchema7 */
-/** @typedef {import("ajv").ErrorObject} ErrorObject */
-/**
- * @typedef {Object} Extend
- * @property {number=} formatMinimum
- * @property {number=} formatMaximum
- * @property {boolean=} formatExclusiveMinimum
- * @property {boolean=} formatExclusiveMaximum
- * @property {string=} link
- * @property {boolean=} undefinedAsNull
- */
-/** @typedef {(JSONSchema4 | JSONSchema6 | JSONSchema7) & Extend} Schema */
-/** @typedef {ErrorObject & { children?: Array<ErrorObject>}} SchemaUtilErrorObject */
-/**
- * @callback PostFormatter
- * @param {string} formattedError
- * @param {SchemaUtilErrorObject} error
- * @returns {string}
- */
-/**
- * @typedef {Object} ValidationErrorConfiguration
- * @property {string=} name
- * @property {string=} baseDataPath
- * @property {PostFormatter=} postFormatter
- */
 /**
  * @param {Schema} schema
  * @param {Array<object> | object} options
