49af9226eb84c6b4d7d4d49d113a6f436dc214ed
Alexander Akait <4567934+alexander-akait@users.noreply.github.com>
2025-10-02 15:11:56 +0300

    fix: reexport `ValidationErrorConfiguration` type (#204)

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 /** @typedef {import("./validate").JSONSchema6} JSONSchema6 */
 /** @typedef {import("./validate").JSONSchema7} JSONSchema7 */
 /** @typedef {import("./validate").ExtendedSchema} ExtendedSchema */
+/** @typedef {import("./validate").ValidationErrorConfiguration} ValidationErrorConfiguration */
 
 const {
   validate,
